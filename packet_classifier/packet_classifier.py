6e367e33ec31b6618cc1166db11ac8ce32b66b21
Avinash sridharan <avinash.sridharan@gmail.com>
2013-07-30 11:45:25 -0700

    Initial infra commit

--- packet_classifier/packet_classifier.py
+++ packet_classifier/packet_classifier.py
@@ -5,56 +5,42 @@ import string
 import time
 import socket
 import struct
-
-#Decoding an IP packet
-def decode_ip_packet(s):
-    d={}
-    d['version']=(ord(s[0]) & 0xf0) >> 4
-    d['header_len']=ord(s[0]) & 0x0f
-    d['tos']=ord(s[1])
-    d['total_len']=socket.ntohs(struct.unpack('H',s[2:4])[0])
-    d['id']=socket.ntohs(struct.unpack('H',s[4:6])[0])
-    d['flags']=(ord(s[6]) & 0xe0) >> 5
-    d['fragment_offset']=socket.ntohs(struct.unpack('H',s[6:8])[0] & 0x1f)
-    d['ttl']=ord(s[8])
-    d['protocol']=ord(s[9])
-    d['checksum']=socket.ntohs(struct.unpack('H',s[10:12])[0])
-    d['source_address']=pcap.ntoa(struct.unpack('i',s[12:16])[0])
-    d['destination_address']=pcap.ntoa(struct.unpack('i',s[16:20])[0])
-    if d['header_len']>5:
-        d['options']=s[20:4*(d['header_len']-5)]
-    else:
-        d['options']=None
-    d['data']=s[4*d['header_len']:]
-    return d
+import operator
+import pywt
+from net.ip.ip_packet import IP_packet
+from net.tcp.tcp_packet import TCP_packet 
+from flows.flow_table import Flow_table
+from filters.pkt_filter import Pkt_filter
 
 
 if __name__=='__main__':
+	#session table.
+	flow_table = Flow_table()
 	#Create the pcap object
 	p = pcap.pcapObject()
+	pkt_filter = Pkt_filter()
 	#Open the dump file
-	p.open_offline('/Users/asridharan/netflix_dump.pcap')
-	i = 0
-	total_len = 0
-	while 1:
-		i +=1
-		try:
-			#p.next() returns a 3 tuple. Length, Data, Time stamp
-			pkt = p.next()
-			length= pkt[0]
-			total_len +=length
-			data = pkt[1]
-			time_stamp = pkt[2]
-			if data[12:14]=='\x08\x00':
-				pkt_ip = decode_ip_packet(data[14:])
-				#dump the decoded IP packet
-				#print "Version:"+str(pkt_ip['version'])+'\t'
-				#print "Protocol:"+str(pkt_ip['protocol'])+'\t'
-				#print "Source Address:"+str(pkt_ip['source_address'])+'\t'
-				#print "Destination Addres:"+str(pkt_ip['destination_address'])+'\n'
-		except TypeError:
-			print "Reached end of file, total packets:" + str(i)+"Total length:"+str(total_len)
+	p.open_offline('/Users/asridharan/netflix_24Jul2013.pcap')
+	pkt = p.next()
+	while (pkt != None):
+		length= pkt[0]
+		if (length == 0):
+			print "found 0 length packet"
 			break
+		data = pkt[1]
+		time_stamp = pkt[2]
+		print time_stamp
+		if data[12:14]=='\x08\x00':
+			pkt_ip = IP_packet(data[14:])
+		if (pkt_ip.version == 4):
+			flow_table.update_flow_table(pkt_ip)
+			pkt_filter.apply_filter(pkt_ip)
+
+		pkt = p.next()
+
+	flow_table.print_table()
+
+		
 
 
 
